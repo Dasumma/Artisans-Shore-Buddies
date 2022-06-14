73308f9640adfbc9360a2a96511cd2b4dfc44d38
dscott97 <84057059+dscott97@users.noreply.github.com>
2022-06-13 21:05:19 -0400

    multiplayer
    started implementing multiplayer

---
+++ Team Artisans Project/Assets/Scripts/RelayManager.cs
@@ -0,0 +1,12 @@
+
+using Unity.Netcode;
+using UnityEngine
+
+
+public class RelayManager : <RelayManager>
+{
+    [SerializeField]
+    private int maxConnections = 5
+
+    public UnityTransport Transport => NetworkManager.
+}
