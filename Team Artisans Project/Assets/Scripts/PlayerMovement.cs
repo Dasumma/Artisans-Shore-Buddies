6d6ca05fd5777887daacf31b4853bced4601c73f
Mjwagner2277 <73041240+Mjwagner2277@users.noreply.github.com>
2022-06-14 19:59:15 -0400

    Sammy object update
    saves items collected to list

--- Team Artisans Project/Assets/Scripts/PlayerMovement.cs
+++ Team Artisans Project/Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
-    
+
 {
+    public List<string> items;
     public MovementJoystick movementJoystick;
     public float playerSpeed;
     private Rigidbody2D rb;
@@ -18,13 +19,25 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(movementJoystick.joystickVec.y != 0)
+        if (movementJoystick.joystickVec.y != 0)
         {
             rb.velocity = new Vector2(movementJoystick.joystickVec.x * playerSpeed, movementJoystick.joystickVec.y * playerSpeed);
         }
         else
         {
             rb.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Collectable"))
+        {
+            string itemType = collision.gameObject.GetComponent<collectableScript>().itemType;
+            print("Item Collected: " + itemType);
+
+            items.Add(itemType);
+            Destroy(collision.gameObject);
         }
     }
 }
---
+++ Team Artisans Project/Assets/Scripts/collectableScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableScript : MonoBehaviour
+{
+    public string itemType;
+
+}
