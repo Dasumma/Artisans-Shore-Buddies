2e8a7d931a6f0a7122f6798168bcd8b6a27f5653
Dasumma <dasumma1@gmail.com>
2022-06-22 22:15:28 -0400

    MultiSammyLVL1

--- Team Artisans Project/Assets/Scenes/Scripts/CameraController.cs
+++ Team Artisans Project/Assets/Scenes/Scripts/CameraController.cs
@@ -8,12 +8,13 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Sammy(clone)");
     }
 
     // Update is called once per frame
     void Update()
     {
+        player = GameObject.Find("Sammy(Clone)");
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
     }
 }
