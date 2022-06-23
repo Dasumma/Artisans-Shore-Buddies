22121a0c6c29945284496a6be9f09d3e6291cf0f
Mjwagner2277 <73041240+Mjwagner2277@users.noreply.github.com>
2022-06-23 15:23:33 -0400

    Merge conflicts resolved
    Sammy also now moves

--- Team Artisans Project/Assets/Scripts/PlayerMovement.cs
+++ Team Artisans Project/Assets/Scripts/PlayerMovement.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-<<<<<<< Updated upstream
-=======
 using Photon.Pun;
->>>>>>> Stashed changes
+
 
 public class PlayerMovement : MonoBehaviour
 
